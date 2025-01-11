0aaa93fde715a69c13a7d20dd30d2eea5084d806
ThiagoSCarv <141270430+ThiagoSCarv@users.noreply.github.com>
2025-01-11 15:50:59 -0300

    adicionando validação caso o input esteja vazio

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const addIten = document.querySelector("#addIten");
 const form = document.querySelector("form")
 const ol = document.querySelector("ol")
 const button = document.querySelector("#btn-remove")
+const alert = document.querySelector("#div-alert")
+const alertButton = document.querySelector(".btn-remove")
+
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
@@ -11,29 +15,34 @@ form.addEventListener("submit", (event) => {
   const newInput = document.createElement("input")
   const newLabel = document.createElement("label")
   const newButton = document.createElement("button")
-  const alert = document.querySelector("#div-alert")
-  const alertButton = document.querySelector(".btn-remove")
+  try {
+    if(addIten.value === "") {
+      throw new Error("Por favor, preencha o campo!")
+    }else {
+      newDiv.append(newCheckboxImage, newInput, newLabel, newButton)
 
-  newDiv.classList.add("input-checkbox")
-  newCheckboxImage.classList.add("checkbox-image")
-  newInput.setAttribute("type", "checkbox")
-  newButton.classList.add("btn-remove")
+    newIten.append(newDiv)
   
-  newLabel.textContent = addIten.value
-
-  newDiv.append(newCheckboxImage, newInput, newLabel, newButton)
-
-  newIten.append(newDiv)
-
-  ol.append(newIten)
-
-  newButton.addEventListener("click", (event) => {
-    event.preventDefault()
-    newIten.classList.add("remove-iten")
-    alert.classList.remove("remove-iten")
-  })
-  alertButton.addEventListener("click", (event) => {
-    event.preventDefault()
-    alert.classList.add("remove-iten")
-  })
+    ol.append(newIten)
+    
+    newDiv.classList.add("input-checkbox")
+    newCheckboxImage.classList.add("checkbox-image")
+    newInput.setAttribute("type", "checkbox")
+    newButton.classList.add("btn-remove")
+    
+    newLabel.textContent = addIten.value
+  
+    newButton.addEventListener("click", (event) => {
+      event.preventDefault()
+      newIten.classList.add("remove-iten")
+      alert.classList.remove("remove-iten")
+    })
+    alertButton.addEventListener("click", (event) => {
+      event.preventDefault()
+      alert.classList.add("remove-iten")
+    })
+    }
+  } catch (error) {
+    console.log(error)
+  }
 })
